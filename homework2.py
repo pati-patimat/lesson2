68aa5d8bea9799207184b7d39ce823d4f53d6b06
pati-patimat <patya.khaib@yandex.ru>
2024-07-29 17:17:53 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
 number_of_completed_homework = 12
 number_of_hours = 1.5
 course_name = 'Python'
-time_for_one_task = number_of_hours % number_of_completed_homework #не могу понять, почему в результате 1,5, когда при делении с остатком 1,5 на 12 должно выйти 0,125
+time_for_one_task = number_of_hours / number_of_completed_homework #не могу понять, почему в результате 1,5, когда при делении с остатком 1,5 на 12 должно выйти 0,125
 print('Курс:', course_name,', всего задач:', number_of_completed_homework,', среднее время выполнения:', time_for_one_task )
 
